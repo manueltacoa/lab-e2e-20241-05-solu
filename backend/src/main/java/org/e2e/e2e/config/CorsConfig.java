fa9c945ab6c97e449c4993bb6c5d236009d05aaa
Jeffrey Monja Castro <jeffrey.monja@utec.edu.pe>
2024-05-21 10:07:00 -0500

    Disable CORS policy

---
+++ backend/src/main/java/org/e2e/e2e/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package org.e2e.e2e.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // Disable Cors Origin to React frontend
+                registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5173");
+            }
+        };
+    }
+}
