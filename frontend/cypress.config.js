b7a56f81a6d8624066b10a86f35f139af5d9b501
Jeffrey Monja Castro <jeffrey.monja@utec.edu.pe>
2024-05-24 14:45:13 -0500

    Add Cypress Reporter json

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -4,5 +4,16 @@ export default defineConfig({
   projectId: "mtt98t",
   e2e: {
     baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
   },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    reportFilename: 'results',
+    overwrite: false,
+    html: false,
+    json: true,
+  }
 });
