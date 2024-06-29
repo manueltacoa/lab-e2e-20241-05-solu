2a30c1c12bd38c739cf00b1f70bc585c23a28b9d
Manuel Taco <manuel.taco@utec.edu.pe>
2024-06-29 01:20:55 -0500

    endpoints p1

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -4,3 +4,18 @@ export default {
     autoprefixer: {},
   },
 }
+
+module.exports = {
+  plugins: {
+    'postcss-preset-mantine': {},
+    'postcss-simple-vars': {
+      variables: {
+        'mantine-breakpoint-xs': '36em',
+        'mantine-breakpoint-sm': '48em',
+        'mantine-breakpoint-md': '62em',
+        'mantine-breakpoint-lg': '75em',
+        'mantine-breakpoint-xl': '88em',
+      },
+    },
+  },
+};
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,40 @@
-import './styles/App.css'
-import { 
-	BrowserRouter as Router, 
-	Routes, 
-	Route,
-	Navigate } from 'react-router-dom'
-import { Navbar } from './layout/Navbar'
-import { Login } from './pages/Login'
-import { Register } from './pages/Register'
-import { Dashboard } from './pages/Dashboard'
-import { EditProfile } from './pages/EditProfile'
-import { EditVehicle } from './pages/EditVehicle'
-import { NotFound } from './pages/NotFound'
+import "./styles/App.css";
+import "@mantine/core/styles.css";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Navbar } from "./layout/Navbar";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Dashboard } from "./pages/Dashboard";
+import { EditProfile } from "./pages/EditProfile";
+import { EditVehicle } from "./pages/EditVehicle";
+import { NotFound } from "./pages/NotFound";
+import { MantineProvider } from "@mantine/core";
 
 function App() {
-  
   return (
     <>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Navigate to="/auth/login"/>} />
-          <Route path="/auth/login" element={<Login/>} />
-          <Route path="/auth/register" element={<Register/>} />
-          <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/profile/edit" element={<EditProfile/>} />
-          <Route path="/vehicle/edit" element={<EditVehicle/>} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
+      <MantineProvider defaultColorScheme="dark">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Navigate to="/auth/login" />} />
+            <Route path="/auth/login" element={<Login />} />
+            <Route path="/auth/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile/edit" element={<EditProfile />} />
+            <Route path="/vehicle/edit" element={<EditVehicle />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </MantineProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
